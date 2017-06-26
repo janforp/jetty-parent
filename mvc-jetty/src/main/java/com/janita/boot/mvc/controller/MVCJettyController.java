d7d0987635fcaa9f5ca07702aaa17fe2212b6cfd
janita <zcj880902>
2017-06-26 12:56:59 +0800

    mvc

---
+++ mvc-jetty/src/main/java/com/janita/boot/mvc/controller/MVCJettyController.java
@@ -0,0 +1,8 @@
+package com.janita.boot.mvc.controller;
+
+/**
+ * Created by Janita on 2017/6/26 0026-下午 12:56
+ * 该类是：
+ */
+public class MVCJettyController {
+}
---
+++ mvc-jetty/src/main/java/com/janita/boot/mvc/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Janita on 2017/6/26 0026-下午 12:56
+ * 该类是：
+ */
+package com.janita.boot.mvc;
